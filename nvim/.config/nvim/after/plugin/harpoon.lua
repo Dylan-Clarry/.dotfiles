ae99e481947fafa80b86d3c0fb3d61db13a4ef74
Dylan <dylan.imail@gmail.com>
2022-09-26 14:28:12 -0400

    harpoon deleted unusable keybind

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -7,4 +7,3 @@ nnoremap("<C-h>", function() require("harpoon.ui").toggle_quick_menu() end, sile
 nnoremap("<C-j>", function() require("harpoon.ui").nav_file(1) end, silent)
 nnoremap("<C-k>", function() require("harpoon.ui").nav_file(2) end, silent)
 nnoremap("<C-l>", function() require("harpoon.ui").nav_file(3) end, silent)
-nnoremap("<C-;>", function() require("harpoon.ui").nav_file(4) end, silent)
