19c42277563053d7bf5ab4cba1b41cc64ab27e14
Dylan <dylan.imail@gmail.com>
2023-03-09 21:13:38 -0500

    incremental selection

--- nvim/.config/nvim/after/plugin/treesitter.rc.lua
+++ nvim/.config/nvim/after/plugin/treesitter.rc.lua
@@ -6,5 +6,14 @@ configs.setup {
     },
     indent = {
       enable = false, -- default is disabled anyways
-    }
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<c-space>',
+            node_incremental = '<c-space>',
+            scope_incremental = '<c-s>',
+            node_decremental = '<c-backspace>',
+        },
+    },
 }
